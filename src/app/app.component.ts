fd7af7413e17cceaf4b19885b61820e80144d550
J Russ <jtr03@sbcglobal.net>
2022-10-21 22:50:49 -0400

    further changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-github-actions';
+  title = 'J Russ Portfolio';
 }
