1b2cc729995458d21a117774645db87b4f7be397
J Russ <jtr03@sbcglobal.net>
2022-10-24 12:02:05 -0400

    initial bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FootbarComponent } from './footbar/footbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    FootbarComponent,
   ],
   imports: [
     BrowserModule,
